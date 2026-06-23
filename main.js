864291799a65979256b1c09977f8b6c0a2f812d6
stefanisilva <stefani.arsilva@senacsp.edu.br>
2026-06-23 20:28:30 -0300

    Refactor code structure for improved readability and maintainability

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const { app, BrowserWindow } = require("electron")
+const path = require('path')
+
+if (process.platform === 'win32') {
+    app.setAppUserModelId('com.electron.appstreaming')
+}
 
 function criarJanela() {
+    
+
     const janela = new BrowserWindow({
         width: 1200,
-        height: 800
+        height: 800,
+        icon: path.join(__dirname, 'app-imagens', 'logo.png'),
     })
 
     janela.loadFile("public/login.html")
